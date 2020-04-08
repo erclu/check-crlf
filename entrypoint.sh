576a9b968202b17e8ac241d573341e8c9d3415c5
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-04-08 15:05:44 +0200

    Made an exlude option

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,24 @@ BOLD_RED='\033[1;31m'
 BOLD_GREEN='\033[1;32m'
 NC='\033[0m'
 
-ERROR_COUNT=0
+FILES_TYPES="$(\
+    find . ! -path "./.git/*" -not -type d  -exec file "{}" ";"
+)"
 
-if \
-  grep \
-    --recursive \
-    --files-with-matches \
-    --binary \
-    --binary-files=without-match \
-    --max-count=1 \
-    --exclude-dir="\.git" \
-    $'\r' \
-    $1 \
-  ; then
-  # TODO exit status should be number of files with wrong endings found
-  echo -e "${BOLD_RED}Found at least a file with CRLF endings.${NC}"
-  exit 1
+FILES_WITH_CRLF=$(echo "$FILES_TYPES" | grep " CRLF " | cut -d " " -f 1 |cut -d ":" -f 1)
+
+for word in $2
+do
+    FILES_WITH_CRLF=$(echo "$FILES_WITH_CRLF" | grep "^./$word" --invert-match)
+done
+
+if [ -z "$FILES_WITH_CRLF" ]
+then
+    echo -e "${BOLD_GREEN}No files with CRLF endings found.${NC}"
+    exit 0
+else
+    NR_FILES=$(echo "$FILES_WITH_CRLF" |wc -l)
+    echo -e "${BOLD_RED}Found $NR_FILES files with CRLF endings.${NC}"
+    echo "$FILES_WITH_CRLF"
+    exit $NR_FILES
 fi
-
-echo -e "${BOLD_GREEN}No files with CRLF endings found.${NC}"
-exit 0
