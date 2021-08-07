e88f7d8ab1ecf24da8381b99be5678ea7cdc80e7
Luca Ercole <30255227+erclu@users.noreply.github.com>
2021-08-07 15:48:35 +0200

    a bunch of fixes, refactorings, improvements and probably some bugs too

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-printf "searching for CRLF endings in: $1\n"
-# TODO allow multiple paths with $@ instead of $1
+FOLDER=$1
+EXCLUDE=$2
+echo "searching for CRLF endings in: $FOLDER"
+
+FILES_TYPES="$(find "$FOLDER" ! -path "./.git/*" -not -type d -exec file "{}" ";")"
+FILES_WITH_CRLF=$(echo "$FILES_TYPES" | grep " CRLF " | cut -d " " -f 1 | cut -d ":" -f 1)
+
+for word in $EXCLUDE; do
+  FILES_WITH_CRLF=$(echo "$FILES_WITH_CRLF" | grep --invert-match -E "$word")
+done
 
 BOLD_RED='\033[1;31m'
 BOLD_GREEN='\033[1;32m'
-NC='\033[0m'
+NO_COLOR='\033[0m'
 
-FILES_TYPES="$(\
-    find . ! -path "./.git/*" -not -type d  -exec file "{}" ";"
-)"
-
-FILES_WITH_CRLF=$(echo "$FILES_TYPES" | grep " CRLF " | cut -d " " -f 1 | cut -d ":" -f 1)
-
-for word in $2
-do
-    FILES_WITH_CRLF=$(echo "$FILES_WITH_CRLF" | grep "^./$word" --invert-match)
-done
-
-if [ -z "$FILES_WITH_CRLF" ]
-then
-    printf "${BOLD_GREEN}No files with CRLF endings found.${NC}"
-    exit 0
+if [ -z "$FILES_WITH_CRLF" ]; then
+  echo -e "${BOLD_GREEN}No files with CRLF endings found.${NO_COLOR}"
+  exit 0
 else
-    NR_FILES=$(echo "$FILES_WITH_CRLF" | wc -l)
-    printf "${BOLD_RED}Found $NR_FILES files with CRLF endings.${NC}"
-    printf "$FILES_WITH_CRLF"
-    exit $NR_FILES
+  NR_FILES=$(echo "$FILES_WITH_CRLF" | wc -l)
+  echo -e "${BOLD_RED}Found ${NR_FILES} files with CRLF endings.${NO_COLOR}"
+  echo "$FILES_WITH_CRLF"
+  exit "$NR_FILES"
 fi
